2925429a2c80e6dd74cff3db02e69ada99ecafb5
Acshi Haggenmiller <acshikh@gmail.com>
2016-06-15 13:48:09 -0400

    removed categories to replace with a private/public topic system and tags. not complete.

--- website/settings/defaults.py
+++ website/settings/defaults.py
@@ -1780,6 +1780,7 @@ DISCOURSE_SERVER_SETTINGS = {'title': 'Open Science Framework',
                              'title_min_entropy': '0',
                              'title_prettify': 'false',
                              'allow_duplicate_topic_titles': 'true',
+                             'tagging_enabled': 'true',
                              }
 
 DISCOURSE_SERVER_CUSTOMIZATIONS = [{'name': 'MFR',
